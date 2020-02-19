b8a1faa400413d077d33cec52f68d61f5ee1aef7
perillaroc <perillaroc@gmail.com>
2020-02-19 22:04:06 +0800

    :art: use Enum as DFA's state.

--- nwpc_workflow_log_model/analytics/node_situation_dfa.py
+++ nwpc_workflow_log_model/analytics/node_situation_dfa.py
@@ -8,7 +8,7 @@ from transitions import Machine
 
 
 class NodeSituationDFA(object):
-    states = [e.value for e in SituationType]
+    states = [e for e in SituationType]
 
     def __init__(self, name):
         self.name = name
@@ -16,7 +16,7 @@ class NodeSituationDFA(object):
         self.machine = Machine(
             model=self.node_situation,
             states=NodeSituationDFA.states,
-            initial=SituationType.Initial.value,
+            initial=SituationType.Initial,
         )
 
         self._initial_transitions()
@@ -31,11 +31,11 @@ class NodeSituationDFA(object):
         self._initial_transitions_for_active()
 
     def _initial_transitions_for_init(self):
-        source = SituationType.Initial.value
+        source = SituationType.Initial
         self.machine.add_transition(
             trigger=NodeStatus.queued.value,
             source=source,
-            dest=SituationType.CurrentQueue.value,
+            dest=SituationType.CurrentQueue,
         )
         self.machine.add_transition(
             trigger=NodeStatus.complete.value,
@@ -44,43 +44,43 @@ class NodeSituationDFA(object):
         )
 
     def _initial_transitions_for_current_queue(self):
-        source = SituationType.CurrentQueue.value
+        source = SituationType.CurrentQueue
         self.machine.add_transition(
             trigger=NodeStatus.submitted.value,
             source=source,
-            dest=SituationType.Submit.value,
+            dest=SituationType.Submit,
         )
         self.machine.add_transition(
             trigger=NodeStatus.aborted.value,
             source=source,
-            dest=SituationType.Error.value,
+            dest=SituationType.Error,
         )
         self.machine.add_transition(
             trigger=NodeStatus.active.value,
             source=source,
-            dest=SituationType.Active.value,
+            dest=SituationType.Active,
         )
         self.machine.add_transition(
             trigger=NodeStatus.complete.value,
             source=source,
-            dest=SituationType.Unknown.value,
+            dest=SituationType.Unknown,
         )
         self.machine.add_transition(
             trigger=NodeStatus.queued.value,
             source=source,
-            dest=SituationType.Unknown.value,
+            dest=SituationType.Unknown,
         )
 
     def _initial_transitions_for_submit(self):
         self.machine.add_transition(
             trigger=NodeStatus.active.value,
-            source=SituationType.Submit.value,
-            dest=SituationType.Active.value,
+            source=SituationType.Submit,
+            dest=SituationType.Active,
         )
 
     def _initial_transitions_for_active(self):
         self.machine.add_transition(
             trigger=NodeStatus.complete.value,
-            source=SituationType.Active.value,
-            dest=SituationType.Complete.value,
+            source=SituationType.Active,
+            dest=SituationType.Complete,
         )
--- tests/analytics/node_situation/test_node_situation.py
+++ tests/analytics/node_situation/test_node_situation.py
@@ -7,13 +7,13 @@ from nwpc_workflow_log_model.analytics.node_situation_dfa import (
 def test_node_situation_dfa():
     dfa = NodeSituationDFA(name="test")
     dfa.trigger(NodeStatus.queued.value)
-    assert dfa.node_situation.state == SituationType.CurrentQueue.value
+    assert dfa.node_situation.state == SituationType.CurrentQueue
 
     dfa.trigger(NodeStatus.submitted.value)
-    assert dfa.node_situation.state == SituationType.Submit.value
+    assert dfa.node_situation.state == SituationType.Submit
 
     dfa.trigger(NodeStatus.active.value)
-    assert dfa.node_situation.state == SituationType.Active.value
+    assert dfa.node_situation.state == SituationType.Active
 
     dfa.trigger(NodeStatus.complete.value)
-    assert dfa.node_situation.state == SituationType.Complete.value
+    assert dfa.node_situation.state == SituationType.Complete
