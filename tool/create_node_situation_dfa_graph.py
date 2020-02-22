277df013c87c7abb5b1126e0b176ca61143a13c4
perillaroc <perillaroc@gmail.com>
2020-02-22 12:31:59 +0800

    :art: update CLI for DFA graph.

--- tool/create_node_situation_dfa_graph.py
+++ tool/create_node_situation_dfa_graph.py
@@ -1,5 +1,14 @@
+import click
 from nwpc_workflow_log_model.analytics.node_situation_dfa import NodeSituationDFA
 
-dfa = NodeSituationDFA("ecflow")
 
-dfa.node_situation.get_graph().draw('my_state_diagram.png', prog='dot')
+@click.command()
+@click.option("--name", default="ecflow", help="DFA's name")
+@click.option("--output-file", help="output file path", required=True)
+def cli(name, output_file):
+    dfa = NodeSituationDFA(name)
+    dfa.node_situation.get_graph().draw(output_file, prog='dot')
+
+
+if __name__ == "__main__":
+    cli()
