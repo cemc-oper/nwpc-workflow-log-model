f804eea939c8340ba559f6c9c3d6c702a4c54211
perillaroc <perillaroc@gmail.com>
2020-06-14 10:28:20 +0800

    :art: add type hints.

--- nwpc_workflow_log_model/log_record/ecflow/record.py
+++ nwpc_workflow_log_model/log_record/ecflow/record.py
@@ -1,3 +1,5 @@
+import datetime
+
 from ..log_record import LogRecord, LogType
 from .util import EventType
 
@@ -5,16 +7,18 @@ from .util import EventType
 class EcflowLogRecord(LogRecord):
     def __init__(
             self,
-            log_type=LogType.Unknown,
-            date=None,
-            time=None,
-            log_record=None):
+            log_type: LogType = LogType.Unknown,
+            date: datetime.date = None,
+            time: datetime.time = None,
+            log_record: str = None):
         LogRecord.__init__(self)
         self.log_type = log_type
         self.log_record = log_record
         self.date = date
         self.time = time
+
+        # ecflow log event type
         self.event_type: EventType = EventType.Unknown
 
     def parse(self, line: str):
-        raise NotImplemented("Please use EcflowLogParser to parse logs.")
+        raise NotImplemented("Please use EcflowLogParser to parse ecflow logs.")
